5aaa2b0e50b6047af55662512a70483dde9dcdca
mykhailo-ros <mykhailo.rosliakov@gmail.com>
2026-01-16 00:05:53 +0200

    Main script v0.1 was implemented.

---
+++ albumfetch.zsh
@@ -0,0 +1,39 @@
+#!/usr/bin/env zsh
+
+albumfetch() {
+
+    setopt local_options extended_glob null_glob
+
+    local logos_dir="${HOME}/.config/fastfetch/logos"
+    local default_logo="${logos_dir}/_default.png" 
+    local logo=""
+
+    local album_now=$(playerctl -p spotify metadata album 2>/dev/null)
+    local status=$(playerctl status 2>/dev/null)
+
+    if [[ -n "$album_now" && ("$status" == "Playing" || "$status" == "Paused") ]]; then
+        
+        local clean_name="${album_now%% \(*}"
+        clean_name="${${clean_name}//[ >]/_}"
+
+        # echo "$clean_name"  #debug
+
+        local match=($logos_dir/(#i)${clean_name}.*)
+        if (( $#match > 0 )); then
+            logo="${match[1]}"
+        fi
+    fi
+
+    if [[ -z "$logo" ]]; then     # no match = random logo
+        local all_logos=($logos_dir/*(N))
+        if (( $#all_logos > 0 )); then
+            logo="${all_logos[$((RANDOM % $#all_logos + 1))]}"
+        fi
+    fi
+
+    if [[ -f "$logo" ]]; then
+        fastfetch --logo "$logo" --logo-type chafa --logo-width 35
+    else
+        fastfetch
+    fi
+}
